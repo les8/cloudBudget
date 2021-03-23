b64fb6674f79cf05b17348a16315143709f1ede1
les8 <leonid.skurihin@yandex.ru>
2021-03-23 08:27:06 +0300

    upgrade popup

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-const link = document.getElementById("popup-js__link")
+let link = document.getElementById("popup-js__link")
+let link2 = document.getElementById("popup-js__link2")
+let title = document.getElementById("popup-title")
+let text = document.getElementById("popup-text")
 const area = document.getElementById("popup-area")
 const close = document.getElementById("popup-close")
 let pop = document.getElementById("popup-js")
 let cont = document.getElementById("popup-content")
 
-link.addEventListener("click", showPopUp)
+
+link.addEventListener("click", function() {showPopUp(1)})
+link2.addEventListener("click", function() {showPopUp(2)})
 area.addEventListener("click", closePopUp)
 close.addEventListener("click", closePopUp)
 
-function showPopUp() {
+function showPopUp(parameter) {
     pop.style.visibility = "visible"
     pop.style.opacity = "1"
     cont.style.opacity = "1"
@@ -17,6 +22,13 @@ function showPopUp() {
             closePopUp()
         }
     })
+    if (parameter == 1){
+        title.innerHTML = "Заголовок 1"
+        text.innerHTML = "Текст 1"
+    } else if (parameter == 2) {
+        title.innerHTML = "Заголовок 2"
+        text.innerHTML = "Текст 2"
+    } else return           
 }
 
 function closePopUp() {
